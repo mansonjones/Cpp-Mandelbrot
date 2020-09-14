2e88d2fa0b058f940fe4dc91de9e27eb36dc89aa
Manson Jones <manson.jones@gmail.com>
2020-09-14 11:12:48 -0700

    separated gui starter code into .h and .cpp files

---
+++ src/mandelbrot_gui.h
@@ -0,0 +1,45 @@
+#ifndef MANDELBROT_GUI_H_
+#define MANDELBROT_GUI_H_
+
+#include <wx/wx.h>
+#include <wx/sizer.h>
+
+class MandelbrotPanel : public wxPanel
+{
+    wxImage image;
+    wxBitmap resized;
+    int w, h;
+    
+public:
+    MandelbrotPanel(wxFrame* parent, wxString file, wxBitmapType format);
+    
+    void paintEvent(wxPaintEvent & evt);
+    void paintNow();
+    void OnSize(wxSizeEvent& event);
+    void render(wxDC& dc);
+    
+    // some useful events
+    /*
+     void mouseMoved(wxMouseEvent& event);
+     void mouseDown(wxMouseEvent& event);
+     void mouseWheelMoved(wxMouseEvent& event);
+     void mouseReleased(wxMouseEvent& event);
+     void rightClick(wxMouseEvent& event);
+     void mouseLeftWindow(wxMouseEvent& event);
+     void keyPressed(wxKeyEvent& event);
+     void keyReleased(wxKeyEvent& event);
+     */
+    
+    DECLARE_EVENT_TABLE()
+};
+
+class MandelbrotApp: public wxApp
+{
+    
+    wxFrame *frame;
+    MandelbrotPanel * drawPane;
+public:
+    bool OnInit();
+};
+
+#endif // MANDELBROT_GUI_H_
