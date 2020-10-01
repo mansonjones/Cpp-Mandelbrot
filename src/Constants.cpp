f6c67c399d0a8b4ad191d2efd532a4aed0bc0ec8
Manson Jones <manson.jones@gmail.com>
2020-10-01 12:26:21 -0700

    added new files for managing defaults

---
+++ src/Constants.cpp
@@ -0,0 +1,3 @@
+#include "Constants.h"
+
+const std::string Constants::defaultFileName("mandelbrot_01.pm");
---
+++ src/Constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#include <string>
+
+struct Constants
+{
+public:
+    static const std::string defaultFileName;
+};
+
+#endif // CONSTANTS_H_
